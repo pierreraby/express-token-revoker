ed14e062fd88d6167b8a92d75c80a54844fc5f0a
pierreraby <pierre@pierreraby.dev>
2025-01-03 02:37:33 +0100

    Refactor revoker middleware configuration by removing redundant token type definitions and streamlining opaque token settings

--- example/middlewares/revoker.js
+++ example/middlewares/revoker.js
@@ -6,18 +6,16 @@ const JWTconfig = { // this config uses ~21MB of memory
   numItems: 1000000, // 1 million items
   fpRate: 0.000001, // one in a million
   rotateTime: 1800000, // 30 minutes
-  type: "jwt", // type of token to check
   claimsToCheck: ["jti", "fam", "sub"] // adding claims to check, return a JWT check middleware
 };
 
 const JWTrevoker = new Revoker(JWTconfig);
 const JWTfilter = JWTrevoker.getMiddleware();
 
-const opaqueConfig = { // this config uses ~21MB of memory
-  numItems: 1000000, // 1 million items
-  fpRate: 0.000001, // one in a million
-  rotateTime: 1800000, // 30 minutes
-  type: "opaque", // type of token to check
+const opaqueConfig = {
+  numItems: 1000000,
+  fpRate: 0.000001,
+  rotateTime: 1800000,
   opaqueHeader: "Authorization" // adding header where to check opaque token, maybe 'X-Auth-Token', etc
 };
 
@@ -25,10 +23,8 @@ const opaqueRevoker = new Revoker(opaqueConfig);
 const opaqueFilter = opaqueRevoker.getMiddleware();
 
 const opaqueConfigCustom = { // this config uses ~21MB of memory
-  numItems: 1000000, // 1 million items
-  fpRate: 0.000001, // one in a million
-  rotateTime: 1800000, // 30 minutes
-  type: "opaque", // type of token to check
+  numItems: 1000000,
+  rotateTime: 1800000,
   opaqueHeader: "X-Auth-Token" // adding header where to check opaque token.
 };
 
